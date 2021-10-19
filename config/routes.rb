808cce13a2741b7f1e5674b6637f696564505a29
danilondo08 <londonodaniela0653@gmail>
2021-10-18 19:40:27 -0500

    Finish static-pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get 'static_pages/basics'
+  get 'static_pages/ruby'
+  get 'static_pages/git'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-    root 'application#hello'
+  # root 'application#hello'
 end
